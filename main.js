9b2c86c486a760ee05caa431a13d86ac051ae16f
zhangss <zssqqzss@vip.qq.com>
2017-09-21 20:00:36 +0800

    watch window's position and size

--- main.js
+++ main.js
@@ -1,15 +1,69 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-let win;
+const fs = require("fs");
+let win, x, y, width, height, config;
+//console.log(global);
+
+if (fs.existsSync("./window.json")) {
+    x = require("./window.json").x;
+    y = require("./window.json").y;
+    width = require("./window.json").width;
+    height = require("./window.json").height;
+} else {
+    x = 0;
+    y = 0;
+    width = 400;
+    height = 600;
+    config = {
+        x: x,
+        y: y,
+        width: width,
+        height: height
+    }
+    fs.writeFileSync("./window.json", JSON.stringify(config, null, 1), "utf-8", function() {});
+}
 
 function openWindow() {
-    win = new BrowserWindow({ width: 800, height: 600, frame: false });
+    win = new BrowserWindow({
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+        title: `bangumi`,
+        frame: false
+    });
 
     win.loadURL('file://' + __dirname + '/index.html');
 
     win.on("closed", () => {
-        win = "";
+        win = null;
+    });
+
+    win.on("resize", () => {
+        const { screen } = electron
+        const { workArea } = screen.getPrimaryDisplay();
+        config = {
+            x: win.getPosition()[0],
+            y: win.getPosition()[1],
+            width: win.getSize()[0],
+            height: win.getSize()[1]
+        }
+        fs.writeFileSync("./window.json", JSON.stringify(config, null, 1), "utf-8", function() {})
+        console.log(win.getSize(), win.getPosition());
+    });
+
+    win.on("move", () => {
+        const { screen } = electron
+        const { workArea } = screen.getPrimaryDisplay();
+        config = {
+            x: win.getPosition()[0],
+            y: win.getPosition()[1],
+            width: win.getSize()[0],
+            height: win.getSize()[1]
+        }
+        fs.writeFileSync("./window.json", JSON.stringify(config, null, 1), "utf-8", function() {})
+        console.log(win.getSize(), win.getPosition());
     });
     //win.webContents.openDevTools();
 }
